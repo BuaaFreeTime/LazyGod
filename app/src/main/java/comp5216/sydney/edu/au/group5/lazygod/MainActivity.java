76ca01b799b62994bfecfb3b229c68dc8d199deb
freetime <freetimeliyun@gmail.com>
2019-09-16 19:19:29 +1000

    inital commit

---
+++ app/src/main/java/comp5216/sydney/edu/au/group5/lazygod/MainActivity.java
@@ -0,0 +1,14 @@
+package comp5216.sydney.edu.au.group5.lazygod;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
