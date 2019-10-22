cac21336f203d2543b8b71562c832509417bd0af
O-tianlonghu <tianlonghu7@gmail.com>
2019-10-22 23:27:01 +1100

    new activity

--- app/src/main/java/comp5216/sydney/edu/au/group5/lazygod/MainActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/group5/lazygod/MainActivity.java
@@ -2,7 +2,10 @@ package comp5216.sydney.edu.au.group5.lazygod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+//所有button的响应方法
+    public void onAddItemClick(View view){
+        int id = view.getId(); // 获取button的id
+        switch (id){
+            case R.id.button2:
+                Toast.makeText(MainActivity.this,"you are already in runner", Toast.LENGTH_SHORT).show();
+                break;//已经在主界面只需提示
+            case R.id.button4:
+                startActivity(new Intent(MainActivity.this,MeActivity.class));
+                break;// 点击me button 直接启动MeActivity并跳转至用户界面
+            case R.id.button:
+                Toast.makeText(MainActivity.this,"not work", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(MainActivity.this,"not work 1", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+
 }
---
+++ app/src/main/java/comp5216/sydney/edu/au/group5/lazygod/MeActivity.java
@@ -0,0 +1,13 @@
+package comp5216.sydney.edu.au.group5.lazygod;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me);
+    }
+}
