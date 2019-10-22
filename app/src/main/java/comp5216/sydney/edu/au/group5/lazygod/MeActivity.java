324699fc5484b73149a96a4528a00e37e8667a39
O-tianlonghu <tianlonghu7@gmail.com>
2019-10-22 23:45:00 +1100

    new activity

--- app/src/main/java/comp5216/sydney/edu/au/group5/lazygod/MeActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/group5/lazygod/MeActivity.java
@@ -3,7 +3,7 @@ package comp5216.sydney.edu.au.group5.lazygod;
 import android.app.Activity;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
 public class MeActivity extends Activity {
     @Override
