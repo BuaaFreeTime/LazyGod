4550ee0a767d99736b09e8dea99c9e9d7f892fb8
ehomeaaa2010 <810505023@qq.com>
2019-10-24 05:15:31 +1100

    change AppliedActivity

---
+++ app/src/main/java/comp5216/sydney/edu/au/group5/lazygod/SentActivity.java
@@ -0,0 +1,14 @@
+package comp5216.sydney.edu.au.group5.lazygod;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class SentActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_task_sent);
+    }
+}
