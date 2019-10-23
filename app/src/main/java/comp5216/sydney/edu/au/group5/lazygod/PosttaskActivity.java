801a9c75ab68f3914938334b0df737bddabc13bc
ehomeaaa2010 <810505023@qq.com>
2019-10-24 04:19:31 +1100

    10.24 change

---
+++ app/src/main/java/comp5216/sydney/edu/au/group5/lazygod/PosttaskActivity.java
@@ -0,0 +1,14 @@
+package comp5216.sydney.edu.au.group5.lazygod;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class PosttaskActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_task);
+    }
+}
